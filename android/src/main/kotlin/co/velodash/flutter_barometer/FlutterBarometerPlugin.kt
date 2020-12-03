39365743a25f02c069117ad4720fa0f1a406c9b0
Denny Cheng <cdfq152313@gmail.com>
2020-12-03 13:18:55 +0800

    [Feature] Add Android support

--- android/src/main/kotlin/co/velodash/flutter_barometer/FlutterBarometerPlugin.kt
+++ android/src/main/kotlin/co/velodash/flutter_barometer/FlutterBarometerPlugin.kt
@@ -1,36 +1,91 @@
 package co.velodash.flutter_barometer
 
+import android.content.Context.SENSOR_SERVICE
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
 import androidx.annotation.NonNull
-
 import io.flutter.embedding.engine.plugins.FlutterPlugin
+import io.flutter.plugin.common.EventChannel
 import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
-import io.flutter.plugin.common.PluginRegistry.Registrar
 
 /** FlutterBarometerPlugin */
-class FlutterBarometerPlugin: FlutterPlugin, MethodCallHandler {
-  /// The MethodChannel that will the communication between Flutter and native Android
-  ///
-  /// This local reference serves to register the plugin with the Flutter Engine and unregister it
-  /// when the Flutter Engine is detached from the Activity
-  private lateinit var channel : MethodChannel
+class FlutterBarometerPlugin : FlutterPlugin, MethodCallHandler, EventChannel.StreamHandler, SensorEventListener {
 
-  override fun onAttachedToEngine(@NonNull flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
-    channel = MethodChannel(flutterPluginBinding.binaryMessenger, "flutter_barometer")
-    channel.setMethodCallHandler(this)
-  }
+    private lateinit var methodChannel: MethodChannel
+    private lateinit var eventChannel: EventChannel
+    private var sensorManager: SensorManager? = null
+    private var sensor: Sensor? = null
+    private val sinkMap = mutableMapOf<Int, EventChannel.EventSink>()
 
-  override fun onMethodCall(@NonNull call: MethodCall, @NonNull result: Result) {
-    if (call.method == "getPlatformVersion") {
-      result.success("Android ${android.os.Build.VERSION.RELEASE}")
-    } else {
-      result.notImplemented()
+    override fun onAttachedToEngine(@NonNull flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
+        methodChannel = MethodChannel(flutterPluginBinding.binaryMessenger, "flutter_barometer/method")
+        methodChannel.setMethodCallHandler(this)
+        eventChannel = EventChannel(flutterPluginBinding.binaryMessenger, "flutter_barometer/event")
+        eventChannel.setStreamHandler(this)
+        sensorManager = flutterPluginBinding.applicationContext.getSystemService(SENSOR_SERVICE) as? SensorManager
+        sensor = sensorManager?.getDefaultSensor(Sensor.TYPE_PRESSURE)
     }
-  }
 
-  override fun onDetachedFromEngine(@NonNull binding: FlutterPlugin.FlutterPluginBinding) {
-    channel.setMethodCallHandler(null)
-  }
+    override fun onMethodCall(@NonNull call: MethodCall, @NonNull result: Result) {
+        if (call.method == "isValid") {
+            result.success(sensor != null)
+        } else {
+            result.notImplemented()
+        }
+    }
+
+    override fun onDetachedFromEngine(@NonNull binding: FlutterPlugin.FlutterPluginBinding) {
+        methodChannel.setMethodCallHandler(null)
+        eventChannel.setStreamHandler(null)
+        sensorManager = null
+        sensor = null
+        sinkMap.clear()
+    }
+
+    override fun onListen(arguments: Any?, events: EventChannel.EventSink?) {
+        val index = arguments as? Int
+        val sensor = this.sensor
+        val sensorManager = this.sensorManager
+        if (index == null || events == null || sensor == null || sensorManager == null) {
+            return
+        }
+        val isEmpty = sinkMap.isEmpty()
+        sinkMap[index] = events
+        if (isEmpty) {
+            sensorManager.registerListener(this, sensor, SensorManager.SENSOR_DELAY_NORMAL)
+        }
+    }
+
+    override fun onCancel(arguments: Any?) {
+        val index = arguments as? Int
+        val sensor = this.sensor
+        val sensorManager = this.sensorManager
+        if (index == null || sensor == null || sensorManager == null) {
+            return
+        }
+        sinkMap.remove(index)
+        if (sinkMap.isEmpty()) {
+            sensorManager.unregisterListener(this)
+        }
+    }
+
+    override fun onSensorChanged(event: SensorEvent?) {
+        if (event?.values == null || event.values.isEmpty()) {
+            return
+        }
+
+        val result = hashMapOf<String, Any>(
+                "pressure" to event.values[0],
+                "altitude" to SensorManager.getAltitude(SensorManager.PRESSURE_STANDARD_ATMOSPHERE, event.values.first())
+        )
+        sinkMap.forEach { entry -> entry.value.success(result) }
+    }
+
+    override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
+    }
 }
